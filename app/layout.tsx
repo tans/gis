5a6c18729bedc1178761d9e1624422c5b432a908
tans <tans@imac.local>
2024-12-10 15:24:59 +0800

    -

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,8 @@ const geistMono = localFont({
 
 export const metadata: Metadata = {
   title: "GISer科研数据",
-  description: "地面气象资料高空气象资料海洋气象资料天气雷达资料风云气象卫星国外气象卫星实况和再分析产品数值预报产品天气预报产品天气实况产品台风与海洋预报产品气象服务产品",
+  description:
+    "地面气象资料高空气象资料海洋气象资料天气雷达资料风云气象卫星国外气象卫星实况和再分析产品数值预报产品天气预报产品天气实况产品台风与海洋预报产品气象服务产品",
 };
 
 export default function RootLayout({
@@ -29,10 +30,13 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <div className="max-w-screen-md min-h-screen  mx-auto">
-          {children}
-        </div>
+        <div className="max-w-screen-md min-h-screen  mx-auto">{children}</div>
         <Footer></Footer>
+        <script
+          defer
+          src="https://umami.minapp.xin/script.js"
+          data-website-id="a3ae3165-1eb8-4a41-b452-b4f375594461"
+        ></script>
       </body>
     </html>
   );
